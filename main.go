9ef33859e940f222e55f9c929efedc6c6f18803d
swordde <heavenlydemon2024@gmail.com>
2026-07-15 23:59:40 +0530

    defined a basic interface and the struct  as alram maneger and the Alaram

--- main.go
+++ main.go
@@ -2,25 +2,43 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type Alaram struct {
 	Hour   int
 	minute int
 }
+type Manager struct {
+	alram []Alaram
+}
 type AlaramManeger interface {
-	Createalaram()
+	Createalaram(a Alaram)
+	Schedular()
 }
 
 func main() {
 	H := Alaram{Hour: 10, minute: 10}
 
-	var c AlaramManeger
-	c = &H
-	c.Createalaram()
-	fmt.Println("hello world!!!")
+	c := Manager{}
+	c.Createalaram(H)
+	fmt.Println("Alarams:", c)
+	go c.Schedular()
 }
 
-func (c Alaram) Createalaram() {
-	fmt.Printf("alaram is created at %v hours and %v minute", c.Hour, c.minute)
+func (c *Manager) Createalaram(a Alaram) {
+	c.alram = append(c.alram, a)
+	fmt.Printf("alaram is created at %v hours and %v minute", a.Hour, a.minute)
+}
+
+func (c Manager) Schedular() {
+	now := time.Now()
+	hour := now.Hour()
+	minute := now.Minute()
+	for {
+		d := c.alram[0]
+		if hour == d.Hour && minute == d.Hour {
+			fmt.Printf("The alram is going on pls ")
+		}
+	}
 }
